88a91af5026ac6cb979b68973dc326e8c304ac79
anasalshazly1 <anas.alshazly@yahoo.com>
2022-03-11 16:43:13 +0200

    assinging variables

--- fuzzyFSeverity/fuzzyLogic.py
+++ fuzzyFSeverity/fuzzyLogic.py
@@ -57,3 +57,31 @@ class FuzzyLogic:
 
     def set_bl_high_mid(self, mid):
         self.__bl_high_mid = mid
+
+    def set_bl_high_stop(self, stop):
+        self.__bl_high_stop = stop
+
+    def set_tl_low_start(self, start):
+        self.__tl_low_start = start
+
+    def set_tl_low_mid(self, mid):
+        self.__tl_low_mid = mid
+
+    def set_tl_low_stop(self, stop):
+        self.__tl_low_stop = stop
+
+    def set_tl_nor_start(self, start):
+        self.__tl_nor_start = start
+
+    def set_tl_nor_mid(self, mid):
+        self.__tl_nor_mid = mid
+
+    def set_tl_nor_stop(self, stop):
+        self.__tl_nor_stop = stop
+
+    def set_tl_high_start(self, start):
+        self.__tl_high_start = start
+
+    def set_tl_high_mid(self, mid):
+        self.__tl_high_mid = mid
+
