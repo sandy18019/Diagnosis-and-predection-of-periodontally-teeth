5ab2e5cefdd81a5e51639ce534fb740d8393532b
anasalshazly1 <anas.alshazly@yahoo.com>
2022-03-11 19:19:04 +0200

    main and simulate function + updates

--- fuzzyFSeverity/fuzzyLogic.py
+++ fuzzyFSeverity/fuzzyLogic.py
@@ -6,115 +6,6 @@ from skfuzzy import *
 
 class FuzzyLogic:
 
-    
-
-    def set_cal_low(self, start, mid, stop):
-        self.set_cal_low_start(start)
-        self.set_cal_low_mid(mid)
-        self.set_cal_low_stop(stop)
-
-    def set_cal_nor(self, start, mid, stop):
-        self.set_cal_nor_start(start)
-        self.set_cal_nor_mid(mid)
-        self.set_cal_nor_stop(stop)
-
-    def set_cal_high(self, start, mid, stop):
-        self.set_cal_high_start(start)
-        self.set_cal_high_mid(mid)
-        self.set_cal_high_stop(stop)
-
-    def set_cal(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
-        self.set_cal_low(low_start, low_mid, low_stop)
-        self.set_cal_nor(nor_start, nor_mid, nor_stop)
-        self.set_cal_high(high_start, high_mid, high_stop)
-    
-    def set_bl_low(self, start, mid, stop):
-        self.set_bl_low_start(start)
-        self.set_bl_low_mid(mid)
-        self.set_bl_low_stop(stop)
-
-    def set_bl_nor(self, start, mid, stop):
-        self.set_bl_nor_start(start)
-        self.set_bl_nor_mid(mid)
-        self.set_bl_nor_stop(stop)
-
-    def set_bl_high(self, start, mid, stop):
-        self.set_bl_high_start(start)
-        self.set_bl_high_mid(mid)
-        self.set_bl_high_stop(stop)
-
-    def set_bl(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
-        self.set_bl_low(low_start, low_mid, low_stop)
-        self.set_bl_nor(nor_start, nor_mid, nor_stop)
-        self.set_bl_high(high_start, high_mid, high_stop)
-
-    def set_tl_low(self, start, mid, stop):
-        self.set_tl_low_start(start)
-        self.set_tl_low_mid(mid)
-        self.set_tl_low_stop(stop)
-
-    def set_tl_nor(self, start, mid, stop):
-        self.set_tl_nor_start(start)
-        self.set_tl_nor_mid(mid)
-        self.set_tl_nor_stop(stop)
-
-    def set_tl_high(self, start, mid, stop):
-        self.set_tl_high_start(start)
-        self.set_tl_high_mid(mid)
-        self.set_tl_high_stop(stop)
-
-    def set_tl(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid,high_stop):
-        self.set_tl_low(low_start, low_mid, low_stop)
-        self.set_tl_nor(nor_start, nor_mid, nor_stop)
-        self.set_tl_high(high_start, high_mid, high_stop)
-    
-    def set_pd_low(self, start, mid, stop):
-        self.set_pd_low_start(start)
-        self.set_pd_low_mid(mid)
-        self.set_pd_low_stop(stop)
-
-    def set_pd_nor(self, start, mid, stop):
-        self.set_pd_nor_start(start)
-        self.set_pd_nor_mid(mid)
-        self.set_pd_nor_stop(stop)
-
-    def set_pd_high(self, start, mid, stop):
-        self.set_pd_high_start(start)
-        self.set_pd_high_mid(mid)
-        self.set_pd_high_stop(stop)
-
-    def set_pd(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
-        self.set_pd_low(low_start, low_mid, low_stop)
-        self.set_pd_nor(nor_start, nor_mid, nor_stop)
-        self.set_pd_high(high_start, high_mid, high_stop)
-
-    def set_o_mild(self, start, mid, stop):
-        self.set_o_mild_start(start)
-        self.set_o_mild_mid(mid)
-        self.set_o_mild_stop(stop)
-
-    def set_o_mod(self, start, mid, stop):
-        self.set_o_mod_start(start)
-        self.set_o_mod_mid(mid)
-        self.set_o_mod_stop(stop)
-
-    def set_o_severe(self, start, mid, stop):
-        self.set_o_severe_start(start)
-        self.set_o_severe_mid(mid)
-        self.set_o_severe_stop(stop)
-
-    def set_o_adv(self, start, mid, stop):
-        self.set_o_adv_start(start)
-        self.set_o_adv_mid(mid)
-        self.set_o_adv_stop(stop)
-
-    def set_o(self, mild_start, mild_mid, mild_stop, mod_start, mod_mid, mod_stop, severe_start, severe_mid, severe_stop,adv_start, adv_mid,
-              adv_stop):
-        self.set_o_mild(mild_start, mild_mid, mild_stop)
-        self.set_o_mod(mod_start, mod_mid, mod_stop)
-        self.set_o_severe(severe_start, severe_mid, severe_stop)
-        self.set_o_adv(adv_start, adv_mid, adv_stop)
-
     def __init__(self, cal_start=0, cal_stop=10, bl_start=0, bl_stop=10, tl_start=0, tl_stop=10, pd_start=0, pd_stop=10, o_start=0, o_stop=10):
         """
         constructor that assign the start/stop values 
@@ -153,96 +44,202 @@ class FuzzyLogic:
         self.__cal_low_mid = mid
     def set_cal_low_stop(self, stop):
         self.__cal_low_stop = stop
+    def set_cal_low(self, start, mid, stop):
+        self.set_cal_low_start(start)
+        self.set_cal_low_mid(mid)
+        self.set_cal_low_stop(stop)
+
     def set_cal_nor_start(self, start):
         self.__cal_nor_start = start
     def set_cal_nor_mid(self, mid):
         self.__cal_nor_mid = mid
     def set_cal_nor_stop(self, stop):
         self.__cal_nor_stop = stop
+    def set_cal_nor(self, start, mid, stop):
+        self.set_cal_nor_start(start)
+        self.set_cal_nor_mid(mid)
+        self.set_cal_nor_stop(stop)
+
     def set_cal_high_start(self, start):
         self.__cal_high_start = start
     def set_cal_high_mid(self, mid):
         self.__cal_high_mid = mid
     def set_cal_high_stop(self, stop):
         self.__cal_high_stop = stop
+    def set_cal_high(self, start, mid, stop):
+        self.set_cal_high_start(start)
+        self.set_cal_high_mid(mid)
+        self.set_cal_high_stop(stop)
+
+    def set_cal(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
+        self.set_cal_low(low_start, low_mid, low_stop)
+        self.set_cal_nor(nor_start, nor_mid, nor_stop)
+        self.set_cal_high(high_start, high_mid, high_stop)
+
     def set_bl_low_start(self, start):
         self.__bl_low_start = start
     def set_bl_low_mid(self, mid):
         self.__bl_low_mid = mid
     def set_bl_low_stop(self, stop):
         self.__bl_low_stop = stop
+    def set_bl_low(self, start, mid, stop):
+        self.set_bl_low_start(start)
+        self.set_bl_low_mid(mid)
+        self.set_bl_low_stop(stop)
+
     def set_bl_nor_start(self, start):
         self.__bl_nor_start = start
     def set_bl_nor_mid(self, mid):
         self.__bl_nor_mid = mid
     def set_bl_nor_stop(self, stop):
         self.__bl_nor_stop = stop
+    def set_bl_nor(self, start, mid, stop):
+        self.set_bl_nor_start(start)
+        self.set_bl_nor_mid(mid)
+        self.set_bl_nor_stop(stop)
+
     def set_bl_high_start(self, start):
         self.__bl_high_start = start
     def set_bl_high_mid(self, mid):
         self.__bl_high_mid = mid
     def set_bl_high_stop(self, stop):
         self.__bl_high_stop = stop
+    def set_bl_high(self, start, mid, stop):
+        self.set_bl_high_start(start)
+        self.set_bl_high_mid(mid)
+        self.set_bl_high_stop(stop)
+
+    def set_bl(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
+        self.set_bl_low(low_start, low_mid, low_stop)
+        self.set_bl_nor(nor_start, nor_mid, nor_stop)
+        self.set_bl_high(high_start, high_mid, high_stop)
+
     def set_tl_low_start(self, start):
         self.__tl_low_start = start
     def set_tl_low_mid(self, mid):
         self.__tl_low_mid = mid
     def set_tl_low_stop(self, stop):
         self.__tl_low_stop = stop
+    def set_tl_low(self, start, mid, stop):
+        self.set_tl_low_start(start)
+        self.set_tl_low_mid(mid)
+        self.set_tl_low_stop(stop)
+
     def set_tl_nor_start(self, start):
         self.__tl_nor_start = start
     def set_tl_nor_mid(self, mid):
         self.__tl_nor_mid = mid
     def set_tl_nor_stop(self, stop):
         self.__tl_nor_stop = stop
+    def set_tl_nor(self, start, mid, stop):
+        self.set_tl_nor_start(start)
+        self.set_tl_nor_mid(mid)
+        self.set_tl_nor_stop(stop)
+
     def set_tl_high_start(self, start):
         self.__tl_high_start = start
     def set_tl_high_mid(self, mid):
         self.__tl_high_mid = mid
     def set_tl_high_stop(self, stop):
         self.__tl_high_stop = stop
+    def set_tl_high(self, start, mid, stop):
+        self.set_tl_high_start(start)
+        self.set_tl_high_mid(mid)
+        self.set_tl_high_stop(stop)
+
+    def set_tl(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid,high_stop):
+        self.set_tl_low(low_start, low_mid, low_stop)
+        self.set_tl_nor(nor_start, nor_mid, nor_stop)
+        self.set_tl_high(high_start, high_mid, high_stop)
+
     def set_pd_low_start(self, start):
         self.__pd_low_start = start
     def set_pd_low_mid(self, mid):
         self.__pd_low_mid = mid
     def set_pd_low_stop(self, stop):
         self.__pd_low_stop = stop
+    def set_pd_low(self, start, mid, stop):
+        self.set_pd_low_start(start)
+        self.set_pd_low_mid(mid)
+        self.set_pd_low_stop(stop)
+
     def set_pd_nor_start(self, start):
         self.__pd_nor_start = start
     def set_pd_nor_mid(self, mid):
         self.__pd_nor_mid = mid
     def set_pd_nor_stop(self, stop):
         self.__pd_nor_stop = stop
+    def set_pd_nor(self, start, mid, stop):
+        self.set_pd_nor_start(start)
+        self.set_pd_nor_mid(mid)
+        self.set_pd_nor_stop(stop)
+
     def set_pd_high_start(self, start):
         self.__pd_high_start = start
     def set_pd_high_mid(self, mid):
         self.__pd_high_mid = mid
     def set_pd_high_stop(self, stop):
         self.__pd_high_stop = stop
+    def set_pd_high(self, start, mid, stop):
+        self.set_pd_high_start(start)
+        self.set_pd_high_mid(mid)
+        self.set_pd_high_stop(stop)
+
+    def set_pd(self, low_start, low_mid, low_stop, nor_start, nor_mid, nor_stop, high_start, high_mid, high_stop):
+        self.set_pd_low(low_start, low_mid, low_stop)
+        self.set_pd_nor(nor_start, nor_mid, nor_stop)
+        self.set_pd_high(high_start, high_mid, high_stop)
+
     def set_o_mild_start(self, start):
         self.__o_mild_start = start
     def set_o_mild_mid(self, mid):
         self.__o_mild_mid = mid
     def set_o_mild_stop(self, stop):
         self.__o_mild_stop = stop
+    def set_o_mild(self, start, mid, stop):
+        self.set_o_mild_start(start)
+        self.set_o_mild_mid(mid)
+        self.set_o_mild_stop(stop)
+
     def set_o_mod_start(self, start):
         self.__o_mod_start = start
     def set_o_mod_mid(self, mid):
         self.__o_mod_mid = mid
     def set_o_mod_stop(self, stop):
         self.__o_mod_stop = stop
+    def set_o_mod(self, start, mid, stop):
+        self.set_o_mod_start(start)
+        self.set_o_mod_mid(mid)
+        self.set_o_mod_stop(stop)
+    
     def set_o_severe_start(self, start):
         self.__o_severe_start = start
     def set_o_severe_mid(self, mid):
         self.__o_severe_mid = mid
     def set_o_severe_stop(self, stop):
         self.__o_severe_stop = stop
+    def set_o_severe(self, start, mid, stop):
+        self.set_o_severe_start(start)
+        self.set_o_severe_mid(mid)
+        self.set_o_severe_stop(stop)
+
     def set_o_adv_start(self, start):
         self.__o_adv_start = start
     def set_o_adv_mid(self, mid):
         self.__o_adv_mid = mid
     def set_o_adv_stop(self, stop):
         self.__o_adv_stop = stop
+    def set_o_adv(self, start, mid, stop):
+        self.set_o_adv_start(start)
+        self.set_o_adv_mid(mid)
+        self.set_o_adv_stop(stop)
+
+    def set_o(self, mild_start, mild_mid, mild_stop, mod_start, mod_mid, mod_stop, severe_start, severe_mid, severe_stop,adv_start, adv_mid, adv_stop):
+        self.set_o_mild(mild_start, mild_mid, mild_stop)
+        self.set_o_mod(mod_start, mod_mid, mod_stop)
+        self.set_o_severe(severe_start, severe_mid, severe_stop)
+        self.set_o_adv(adv_start, adv_mid, adv_stop)
+
 
     
     def make_member_functions(self):
@@ -620,6 +617,38 @@ class FuzzyLogic:
             self.__rules.append(eval("rule" + str(i)))
             self.__perio_ctrl = skfuzzy.control.ControlSystem(self.__rules)
             self.__perio_ctrl.view()
+
+    def simulate(self, cal_val, bl_val, pd_val, tl_val):
+        """
+        param cal_val: clinical attachment loss value , int or float
+        param bl_val: bone loss value, int or float
+        param pd_val: probing depth value, int or float
+        param tl_val: teeth loss value, int
+        
+        """
+        perio_ctrl_sil = skfuzzy.control.ControlSystemSimulation(self.__perio_ctrl)
+        perio_ctrl_sil.input['CAL'] = cal_val
+        perio_ctrl_sil.input['BL'] = bl_val
+        perio_ctrl_sil.input['TL'] = tl_val
+        perio_ctrl_sil.input['PD'] = pd_val
+        perio_ctrl_sil.compute()
+        self.res = perio_ctrl_sil.print_state()
+        self.output = list(perio_ctrl_sil.output.items())[0][1]
+        print(self.output)
+        print(perio_ctrl_sil.output)
+        print(self.res)
+
+if __name__ == '__main__':
+    fuzzy_system = FuzzyLogic(0, 7, 0, 100,-1,15, 0, 15, 0, 100)
+    fuzzy_system.set_cal(0, 1, 2, 1, 2, 4, 3, 4,7)
+    fuzzy_system.set_bl(0, 10, 14, 10, 14, 33, 15, 33, 100)
+    fuzzy_system.set_tl(-1, -0.5, 0, -0.5, 0, 4, 3, 4, 15)
+    fuzzy_system.set_pd(0,2,4,2,4,6,5,6,15)
+    fuzzy_system.set_o(0,20,40,20,40,60,50,60,80,70,80,100)
+    fuzzy_system.make_variables()
+    fuzzy_system.make_member_functions()
+    fuzzy_system.make_rules()
+    fuzzy_system.simulate(4,16,15,15)
         
         #RULES TO BE CHECKED WITH EXPERTS
         # rule7 = skfuzzy.control.Rule(self.__cal['low'] &
